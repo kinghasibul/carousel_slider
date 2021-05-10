e44cc50d248efa3cc44624a2f14fb55500f0ed4d
King Hasibul <83134617+kinghasibul@users.noreply.github.com>
2021-05-10 21:06:27 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,1 @@
+$(document).ready(function(){$('.owl-carousel').owlCarousel({margin:15,loop:true,autoplay:true,autoplayTimeout:8000,responsiveClass:true,responsiveRefreshRate:true,responsive:{0:{items:1},768:{items:2},1000:{items:2},1200:{items:3},1920:{items:3}}});new WOW().init()});
